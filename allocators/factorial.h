24bfe0a9ea251255b4ea1f9975b9ca839bf9f0e5
r3d9477 <r3d9477@gmail.com>
2021-05-21 11:07:36 +0300

    hw03

--- allocators/factorial.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int calc_factorial (int N)
-{
-    return ( N < 0 ) ? 0 : ( N == 0 ) ? 1 : ( N * calc_factorial( N-1 ) );
-}
---
+++ ipprint/main.cpp
@@ -0,0 +1,13 @@
+#include "lalloc10.h"
+
+#include <map>
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    
+    
+    
+    return 0;
+}
