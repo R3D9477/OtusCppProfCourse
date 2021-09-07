9ff1e37635d9d20f40d02781d00c307ff8758418
r3d9477 <r3d9477@gmail.com>
2021-09-07 10:45:38 +0300

    eorm

---
+++ orm/lib/eorm/test/test_sqlgen.cpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE test_sqlgen
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_sqlgen)
+
+BOOST_AUTO_TEST_CASE(test_sqlgen)
+{
+
+
+
+    BOOST_CHECK (true);
+}
+
+}
+ 
